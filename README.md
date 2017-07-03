4ab03b0c1b6b4df903071700fa4c6855330bb41c
mishu <mishu@xtelecom.ro>
2017-07-03 14:11:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Image Transformer
+[![Total Downloads](https://poser.pugx.org/laravel-enso/imagetransformer/downloads)](https://packagist.org/packages/laravel-enso/imagetransformer)
+[![Latest Stable Version](https://poser.pugx.org/laravel-enso/imagetransformer/version)](https://packagist.org/packages/laravel-enso/imagetransformer)
+
+File manager dependency for [Laravel Enso](https://github.com/laravel-enso/Enso).
+
+### Note
+
+This package is included in [Laravel Enso Core](https://github.com/laravel-enso/Core).
+
+### Contributions
+
+are welcome
---
+++ src/Classes/ImageTransformer.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace LaravelEnso\ImageTransformer\Classes;
+
+use Illuminate\Http\UploadedFile;
+use ImageOptimizer\OptimizerFactory;
+
+class ImageTransformer
+{
+    private $files;
+
+    public function __construct(array $files)
+    {
+        $this->files = $files;
+    }
+
+    public function optimize()
+    {
+        foreach ($this->files as $file) {
+            $this->optimizeImage($file);
+        }
+
+        return $this;
+    }
+
+    public function resize(int $width, int $height)
+    {
+        foreach ($this->files as $file) {
+            $this->resizeImage($file, $width, $height);
+        }
+
+        return $this;
+    }
+
+    private function optimizeImage(UploadedFile $file)
+    {
+        $validator = \Validator::make(['file' => $file], ['file' => 'image']);
+
+        if ($validator->fails()) {
+            return false;
+        }
+
+        $optimizer = (new OptimizerFactory())->get();
+        $optimizer->optimize($file->getRealPath());
+    }
+
+    private function resizeImage(UploadedFile $file, int $width, int $height)
+    {
+        $image = \Image::make($file->getRealPath());
+
+        if ($image->width() > $width) {
+            $image->resize($width, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+        }
+
+        if  ($image->height() > $height) {
+            $image->resize(null, $height, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+        }
+
+        $image->save($file->getRealPath());
+    }
+}
